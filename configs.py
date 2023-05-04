56ca3ac3e8d1a291261606a5224975a7380d499c
me <pourya90091>
2023-05-04 19:18:58 +0330

    Add README + some description

--- configs.py
+++ configs.py
@@ -1,3 +1,11 @@
+"""
+main_channel is a channel name in telegram that attached in all ChatGPT responses (and also must to subscribe by users).
+bot_username is username of the bot in telegram.
+telegram_token is the bot api token for using telegram api.
+openai_api_key is a api key for using ChatGPT api.
+admins is a list of usernames that can use administrator's options in the bot.
+"""
+
 main_channel = "channelname"
 bot_username = "bot username"
 telegram_token = "TOKEN"
